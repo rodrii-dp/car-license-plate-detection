f7d3c5eef45b71cf8dbf533cf7540898179d4337
Rodrigo <rrddpp1@gmail.com>
2024-11-26 12:02:44 +0100

    escribir en archivos

--- add_missing_data.py
+++ add_missing_data.py
@@ -75,3 +75,17 @@ def interpolate_bounding_boxes(data):
             interpolated_data.append(row)
 
     return interpolated_data
+
+with open('test.csv', 'r') as file:
+    reader = csv.DictReader(file)
+    data = list(reader)
+
+# Interpolate missing data
+interpolated_data = interpolate_bounding_boxes(data)
+
+# Write updated data to a new CSV file
+header = ['frame_nmr', 'car_id', 'car_bbox', 'license_plate_bbox', 'license_plate_bbox_score', 'license_number', 'license_number_score']
+with open('test_interpolated.csv', 'w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=header)
+    writer.writeheader()
+    writer.writerows(interpolated_data)
